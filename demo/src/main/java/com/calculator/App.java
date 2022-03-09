8a801624feddb3f86e50f6222cc982f41f8f53a4
JenesisFWork <jenesisfablework@gmail.com>
2022-03-09 13:24:20 -0800

    Changing the code.
    Using the quick build Maven Project, I change the code from "Hello World!" to the menu to print commands for Chavvi's calculator.  Commands don't function yet.

--- demo/src/main/java/com/calculator/App.java
+++ demo/src/main/java/com/calculator/App.java
@@ -1,13 +1,20 @@
 package com.calculator;
 
 /**
- * Hello world!
+ * Hello World! --> calculator
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "a Enter a value of A" );
+        System.out.println( "b Enter a value of B" );
+        System.out.println( "+ Add" );
+        System.out.println( "- Subtract" );
+        System.out.println( "* Multiply" );
+        System.out.println( "/ Divide" );
+        System.out.println( "c Clear" );
+        System.out.println( "q Quit" );
     }
 }
